c0256181cca1a701330b54968e6bf204567a0534
JoaquinG <jogagu2005@gmail.com>
2024-02-23 14:18:53 +0100

    ¿Nicky nickole si o no?

---
+++ featureA.java
@@ -0,0 +1,16 @@
+public class featureA{
+        public static void main (String [] args){
+                Scanner tc = new Scanner(System.in);
+                String Elec = tc.next();
+                tc.close();
+                switch (Elec){
+                        case "Si", "si", "SI":
+                                System.out.println("Obvio pa, tu sabes ma g.");
+                        break;
+                        default:
+                                System.out.println("Cago en la ma que tronchamulas estas hecho.");
+                        break
+                }
+        }
+}
+
